1000fd65a633d1ddd464bbbd60773ea329a08cb1
Reid Blondell <rebncs@rit.edu>
2024-07-09 15:42:43 -0400

    add osquery

--- osquery_configurations.tf
+++ osquery_configurations.tf
@@ -8,3 +8,8 @@ resource "zentral_osquery_enrollment" "default" {
   configuration_id      = zentral_osquery_configuration.default.id
   meta_business_unit_id = zentral_meta_business_unit.default.id
 }
+
+resource "zentral_osquery_configuration_pack" "default-compliance-checks" {
+  configuration_id = zentral_osquery_configuration.default.id
+  pack_id          = zentral_osquery_pack.compliance-checks.id
+}
